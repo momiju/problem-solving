44e2ad866341a3ba8f325008e7ac923bc265c208
Huiju Oh <116543555+momiju@users.noreply.github.com>
2026-08-19 21:52:31 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 2.07 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -4,18 +4,15 @@ class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
         ArrayList<Integer> list = new ArrayList<>();
 
-        // 오늘 날짜를 숫자로 변환
         String[] t = today.split("\\.");
         int todayDate = Integer.parseInt(t[0]) * 12 * 28 + Integer.parseInt(t[1]) * 28 + Integer.parseInt(t[2]);
 
-        // 개인정보 하나씩 확인
         for(int i = 0; i < privacies.length; i++){
             String[] p = privacies[i].split(" ");
             String[] d = p[0].split("\\.");
 
             int date = Integer.parseInt(d[0]) * 12 * 28 + Integer.parseInt(d[1]) * 28 + Integer.parseInt(d[2]);
 
-            // 약관 개월 수 찾기
             int month = 0;
 
             for(int j = 0; j < terms.length; j++){
@@ -27,16 +24,13 @@ class Solution {
                 }
             }
 
-            // 유효기간 더하기
             date += month * 28;
 
-            // 오늘보다 작거나 같으면 파기
             if(date <= todayDate){
                 list.add(i + 1);
             }
         }
 
-        // ArrayList -> 배열
         int[] answer = new int[list.size()];
 
         for(int i = 0; i < list.size(); i++){
