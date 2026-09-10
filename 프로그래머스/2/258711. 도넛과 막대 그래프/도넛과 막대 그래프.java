a7a9977c46b54a874769b2bc08faff3e4e798888
Huiju Oh <116543555+momiju@users.noreply.github.com>
2026-09-10 23:37:24 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 34.03 ms, Memory: 165 MB -BaekjoonHub

---
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] solution(int[][] edges) {
+        int[] answer = new int[4];
+        
+        int[] inDegree = new int[1000001];
+        int[] outDegree = new int[1000001];
+        
+        for(int i=0;i<edges.length;i++){
+            outDegree[edges[i][0]]++;
+            inDegree[edges[i][1]]++;
+        }
+        
+        for(int i=1;i<=1000000;i++){
+            if(inDegree[i]==0 && outDegree[i]>=2){
+                answer[0] = i;
+            }
+            
+            if(outDegree[i]==0 && inDegree[i]>0){
+                answer[2]++;
+            }
+            
+            if(inDegree[i]>=2 && outDegree[i]==2){
+                answer[3]++;
+            }
+        }
+        
+        answer[1] = outDegree[answer[0]]-answer[2]-answer[3];
+        
+        return answer;
+    }
+}
